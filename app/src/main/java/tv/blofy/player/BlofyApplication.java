e97c90e527dcf74f5c53f5d83ced9c1aaf35eeba
i20sss20-maker <i20sss20@gmail.com>
2026-08-28 02:10:43 +0300

    android: add clean application entry point

---
+++ app/src/main/java/tv/blofy/player/BlofyApplication.java
@@ -0,0 +1,9 @@
+package tv.blofy.player;
+
+import android.app.Application;
+
+public final class BlofyApplication extends Application {
+    @Override public void onCreate() {
+        super.onCreate();
+    }
+}
