416ffe1297884151c3975939eeb16c4d0b0b238e
i20sss20-maker <i20sss20@gmail.com>
2026-08-28 02:55:35 +0300

    feat(next): initialize diagnostics logger

--- app/src/main/java/tv/blofy/player/BlofyApplication.java
+++ app/src/main/java/tv/blofy/player/BlofyApplication.java
@@ -2,8 +2,11 @@ package tv.blofy.player;
 
 import android.app.Application;
 
+import tv.blofy.player.diagnostics.DiagnosticsLog;
+
 public final class BlofyApplication extends Application {
     @Override public void onCreate() {
         super.onCreate();
+        DiagnosticsLog.init(this);
     }
 }
